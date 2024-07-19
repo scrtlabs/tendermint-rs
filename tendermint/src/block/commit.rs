b073874376f6c8e72f890430c0708385e0bf5d5d
Ilya Raykker <ilya.th.kapitonov@gmail.com>
2024-07-19 08:47:04 +0400

    Added proto parsing for commit (from 4019839f)

--- tendermint/src/block/commit.rs
+++ tendermint/src/block/commit.rs
@@ -38,6 +38,8 @@ tendermint_pb_modules! {
     };
     use pb::types::Commit as RawCommit;
 
+    impl tendermint_proto::Protobuf<RawCommit> for Commit {}
+
     impl TryFrom<RawCommit> for Commit {
         type Error = Error;
 
